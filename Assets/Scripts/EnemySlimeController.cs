94e8cc42a28b9d9c6be1b81312daf61cb83506c2
Sebastian-V-Apply-Digital <sebastian.villarroel@applydigital.com>
2025-10-09 16:15:07 -0400

    feat: add more pacakges

---
+++ Assets/Scripts/EnemySlimeController.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class EnemySlimeController : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody;
+    private Vector2 _direction;
+    private const float Velocity = 5.0f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+
+    void Start()
+    {
+        _rigidBody.linearVelocity =  _direction * Velocity;
+        transform.right = _direction;
+        StartCoroutine(CountTo10());
+    }
+
+    IEnumerator CountTo10()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+
+    public void SetDirection(Vector2 newDirection)
+    {
+        _direction = newDirection;
+    }
+    
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+}
